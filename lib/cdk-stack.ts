d84fa7bfbdac1dcee1b69c222ed9712d77be940e
Chris <cfelli@protonmail.com>
2021-02-03 20:53:51 -0800

    launch a t2.mirco free-tier eligible EC2 instance
    I shouldn't hardcode credentials like this

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,9 +1,20 @@
+import ec2 = require('@aws-cdk/aws-ec2');
 import * as cdk from '@aws-cdk/core';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create the instance
+    const instance = new ec2.CfnInstance(this, id, {
+      imageId: "ami-0e999cbd62129e3b1",
+      instanceType: "t2.micro",
+      networkInterfaces: [
+        {
+          deviceIndex: "0",
+          subnetId: "subnet-a86f7ff2"
+        }
+      ],
+    });
   }
 }
